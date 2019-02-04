33f12907f76773d9b3371e5f141cc1d6da3ad2fe
cristian <cristian982940@gmail.com>
2019-02-04 12:38:14 -0500

    install gem postgresql and create databases

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_02_04_170015) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.string "price"
